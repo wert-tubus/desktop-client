24b227b175be93c575d375c7b51f934d7fb94abe
Aleksei V. Simonov <wert001@yandex.ru>
2026-04-06 13:52:48 +0300

    make draft info the same style

--- chogori/src/main/java/ru/wert/tubus/chogori/entities/drafts/info/DraftInfoController.java
+++ chogori/src/main/java/ru/wert/tubus/chogori/entities/drafts/info/DraftInfoController.java
@@ -2,9 +2,11 @@ package ru.wert.tubus.chogori.entities.drafts.info;
 
 import javafx.application.Platform;
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
+import javafx.scene.control.Button;
 import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.AnchorPane;
+import ru.wert.tubus.chogori.statics.AppStatic;
 import ru.wert.tubus.client.entity.models.Draft;
 import ru.wert.tubus.winform.enums.EDraftStatus;
 import ru.wert.tubus.winform.enums.EDraftType;
@@ -13,71 +15,107 @@ import static ru.wert.tubus.winform.statics.WinformStatic.parseLDTtoNormalDate;
 
 public class DraftInfoController {
 
+    @FXML private AnchorPane apInfo;
+
+    @FXML private TextField tfId;
+    @FXML private TextField tfDecNumber;
+    @FXML private TextField tfName;
+    @FXML private TextField tfTypeStr;
+    @FXML private TextField tfStatus;
+    @FXML private TextField tfStatusTime;
+    @FXML private TextField tfSourceFileName;
+    @FXML private TextField tfCreationTime;
+    @FXML private TextField tfSource;
+    @FXML private TextArea taNote;
+
+    @FXML private Button btnOK;
 
     @FXML
-    private AnchorPane apInfo;
+    void initialize() {
 
-    @FXML
-    private Label lblId;
+        // Отложенная установка стиля для внутренних элементов
+        taNote.sceneProperty().addListener((obs, oldScene, newScene) -> {
+            if (newScene != null) {
+                javafx.application.Platform.runLater(() -> {
+                    if (taNote.lookup(".content") != null) {
+                        taNote.lookup(".content").setStyle("-fx-background-color: white;");
+                    }
+                });
+            }
+        });
 
-    @FXML
-    private Label lblDecNumber;
-
-    @FXML
-    private Label lblName;
-
-    @FXML
-    private Label lblSourceFileName;
-
-    @FXML
-    private Label lblCreationTime;
-
-    @FXML
-    private Label lblTypeStr;
-
-    @FXML
-    private Label lblStatus;
-
-    @FXML
-    private Label lblStatusTime;
-
-    @FXML
-    private Label lblSource;
-
-    @FXML
-    private TextArea taNote;
-
-    @FXML
-    void initialize(){
-        taNote.setStyle("-fx--: black; -fx-text-fill: white");
+        btnOK.setOnAction(AppStatic::closeWindow);
     }
 
-    public void init(Draft draft){
+    public void init(Draft draft) {
+        if (draft == null) {
+            clearAllFields();
+            return;
+        }
 
-        lblId.setText(String.valueOf(draft.getId()));
+        // ID
+        tfId.setText(String.valueOf(draft.getId()));
 
-        lblDecNumber.setText(draft.getDecimalNumber());
+        // Дец. номер
+        tfDecNumber.setText(draft.getDecimalNumber() != null ? draft.getDecimalNumber() : "");
 
-        lblName.setText(draft.getName());
+        // Наименование
+        tfName.setText(draft.getName() != null ? draft.getName() : "");
 
-        lblSourceFileName.setText(draft.getInitialDraftName());
+        // Имя файла
+        tfSourceFileName.setText(draft.getInitialDraftName() != null ? draft.getInitialDraftName() : "");
 
-        lblCreationTime.setText(parseLDTtoNormalDate(draft.getCreationTime()) + ", " +
-                draft.getCreationUser().getName());
+        // Время создания (с пользователем)
+        String creationTime = "";
+        if (draft.getCreationTime() != null) {
+            creationTime = parseLDTtoNormalDate(draft.getCreationTime());
+        }
+        String creationUser = draft.getCreationUser() != null ? draft.getCreationUser().getName() : "";
+        tfCreationTime.setText(creationTime + (creationUser.isEmpty() ? "" : ", " + creationUser));
 
+        // Тип/стр
         EDraftType type = EDraftType.getDraftTypeById(draft.getDraftType());
-        lblTypeStr.setText(type.getShortName() + "-" + draft.getPageNumber());
+        String typeStr = "";
+        String pageStr = "";
+        if (type != null) {
+            typeStr = type.getShortName() != null ? type.getShortName() : "";
+        }
+        if (draft.getPageNumber() != null) {
+            pageStr = "-" + draft.getPageNumber();
+        }
+        tfTypeStr.setText(typeStr + pageStr);
 
-        lblStatus.setText(EDraftStatus.getStatusById(draft.getStatus()).getStatusName());
+        // Статус
+        if (draft.getStatus() != null) {
+            String statusName = EDraftStatus.getStatusById(draft.getStatus()).getStatusName();
+            tfStatus.setText(statusName != null ? statusName : "");
+        } else {
+            tfStatus.setText("");
+        }
 
-        lblStatusTime.setText(parseLDTtoNormalDate(draft.getStatusTime()));
+        // Время статуса
+        tfStatusTime.setText(draft.getStatusTime() != null ? parseLDTtoNormalDate(draft.getStatusTime()) : "");
 
-        lblSource.setText(draft.getFolder().toUsefulString());
+        // Комплект (папка)
+        tfSource.setText(draft.getFolder() != null ? draft.getFolder().toUsefulString() : "");
 
-        taNote.setText(draft.getNote());
+        // Примечание
+        taNote.setText(draft.getNote() != null ? draft.getNote() : "");
 
-        Platform.runLater(()->apInfo.requestFocus());
-
+        // Устанавливаем фокус
+        Platform.runLater(() -> apInfo.requestFocus());
     }
 
+    private void clearAllFields() {
+        tfId.clear();
+        tfDecNumber.clear();
+        tfName.clear();
+        tfTypeStr.clear();
+        tfStatus.clear();
+        tfStatusTime.clear();
+        tfSourceFileName.clear();
+        tfCreationTime.clear();
+        tfSource.clear();
+        taNote.clear();
+    }
 }
