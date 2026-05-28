c380e39aa7bc7e057697656caa5e347ab21e9227
Aleksei V. Simonov <wert001@yandex.ru>
2026-05-28 08:31:44 +0300

    privent decimal deleting if passports exist

--- chogori/src/main/java/ru/wert/tubus/chogori/entities/decimals/commands/DecimalDeleteCommand.java
+++ chogori/src/main/java/ru/wert/tubus/chogori/entities/decimals/commands/DecimalDeleteCommand.java
@@ -5,11 +5,14 @@ import ru.wert.tubus.chogori.common.commands.ICommand;
 import ru.wert.tubus.chogori.entities.decimals.Decimal_TableView;
 import ru.wert.tubus.chogori.statics.AppStatic;
 import ru.wert.tubus.client.entity.models.Decimal;
+import ru.wert.tubus.client.entity.models.Passport;
 import ru.wert.tubus.winform.warnings.Warning1;
 
 import java.util.List;
 
 import static ru.wert.tubus.chogori.application.services.ChogoriServices.CH_DECIMALS;
+import static ru.wert.tubus.chogori.application.services.ChogoriServices.CH_PASSPORTS;
+import static ru.wert.tubus.chogori.setteings.ChogoriSettings.CH_DEFAULT_PREFIX;
 import static ru.wert.tubus.winform.warnings.WarningMessages.*;
 
 /**
@@ -37,6 +40,14 @@ public class DecimalDeleteCommand implements ICommand {
         int row = tableView.getItems().lastIndexOf(items.get(0));
 
         for (Decimal item : items) {
+            // Проверка наличия связанных Passport перед удалением
+            if (hasLinkedPassports(item)) {
+                Warning1.create($ATTENTION, $ERROR_WHILE_DELETING_ITEM,
+                        String.format("Десятичный классификатор '%s' нельзя удалить, так как с ним связаны паспорта", item.getName()));
+                log.warn("Невозможно удалить Decimal {}, так как существуют связанные Passport", item.getName());
+                continue; // Пропускаем удаление этого элемента
+            }
+
             try {
                 CH_DECIMALS.delete(item);
                 log.info("Удален десятичный классификатор {}", item.getName());
@@ -49,5 +60,33 @@ public class DecimalDeleteCommand implements ICommand {
 
         tableView.easyUpdate(CH_DECIMALS);
         tableView.getSelectionModel().select(row);
+    }
+
+    /**
+     * Проверяет, есть ли у Decimal связанные Passport с префиксом по умолчанию
+     * @param decimal проверяемый десятичный классификатор
+     * @return true - если есть хотя бы один связанный Passport, false - если нет
+     */
+    private boolean hasLinkedPassports(Decimal decimal) {
+        String decimalName = decimal.getName(); // Например "745222"
+
+        // Ищем все паспорта, содержащие в номере этот decimal
+        List<Passport> passports = CH_PASSPORTS.findAllByText(decimalName);
+
+        if (passports == null || passports.isEmpty()) {
+            return false;
+        }
+
+        // Проверяем, есть ли паспорт с нужным префиксом и номером, начинающимся с decimalName
+        for (Passport passport : passports) {
+            if (passport.getPrefix() != null &&
+                    passport.getNumber() != null &&
+                    CH_DEFAULT_PREFIX.equals(passport.getPrefix()) &&
+                    passport.getNumber().startsWith(decimalName + ".")) {
+                return true;
+            }
+        }
+
+        return false;
     }
 }
